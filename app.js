c32cddfba15bbb6f43523e4b5500d0cd6c910367
Aishwarya Keskar <aishwaryakeskar98@gmail.com>
2020-04-29 19:07:01 +0530

    Modify item deletion in other lists

--- app.js
+++ app.js
@@ -3,8 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const date = require(__dirname + "/date.js");
-
+const _ = require("lodash");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,10 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
-
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://admin-aishwarya:test135@cluster0-n0kpy.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = ({
   name: String
@@ -69,7 +65,7 @@ app.get("/", function(req, res) {
 
 
 app.get("/:customListName", function(req, res) {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList) {
     if(!err) {
@@ -117,16 +113,25 @@ app.post("/", function(req, res){
 app.post("/delete", function(req, res) {
     const checkedItemId = req.body.checkbox;
     const listName = req.body.listName;
+
+    if(listName == "Today") {
+      Item.findByIdAndRemove(checkedItemId, function(err) {
+        if(err) {
+          console.log(err);
+        } else {
+          console.log("Successfully removed item");
+        }
+        res.redirect("/");
+      })
+    } else {
+      List.findOneAndUpdate({name:listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList) {
+        if(!err) {
+          res.redirect("/" + listName);
+        }
+      });
+    }
     
     
-    Item.findByIdAndRemove(checkedItemId, function(err) {
-      if(err) {
-        console.log(err);
-      } else {
-        console.log("Successfully removed item");
-      }
-      res.redirect("/");
-    })
 });
 
 app.get("/work", function(req,res){
